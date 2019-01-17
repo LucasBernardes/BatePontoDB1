6aed9275e8adcb97bf1a04a90a4c673973841a89
Lucas Bernardes <lukazbernardez@gmail.com>
2019-01-17 11:41:29 -0200

    Initial Commit

---
+++ Bate PontoTests/Bate_PontoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bate_PontoTests.swift
+//  Bate PontoTests
+//
+//  Created by Lucas Franco Bernardes on 17/01/19.
+//  Copyright © 2019 Lucas Franco Bernardes. All rights reserved.
+//
+
+import XCTest
+@testable import Bate_Ponto
+
+class Bate_PontoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
