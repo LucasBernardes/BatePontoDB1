df6c9b322ce3a06c9d9f3e45378e50c311792eb1
Lucas Bernardes <bernardeslucasf@gmail.com>
2019-01-29 17:53:50 -0200

    Falta fazer o end game nele o cheque mate

--- Bitcoin DB1/BitcoinViewController.swift
+++ Bitcoin DB1/BitcoinViewController.swift
@@ -8,18 +8,137 @@
 
 import UIKit
 import AAInfographics
+import CoreData
 
-class BitcoinViewController: UIViewController {
+class BitcoinViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cotacoes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        if( !(cell != nil))
+        {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        }
+        var atual = cotacoes[indexPath.row]
+        
+
+        cell!.textLabel?.text = "\(atual.value(forKey: "valor") as! Double)"
+        cell!.textLabel?.textColor = .white
+        cell!.backgroundColor? = .clear
+        return cell!
+    }
+    
+    @IBOutlet weak var customTableView: UITableView!
     public var chartType: AAChartType?
     public var step: Bool?
     private var aaChartModel: AAChartModel?
     private var aaChartView: AAChartView?
     var cotacao: Cotacao?
+    var currentValue = 0.00000
+    var cotacoes: [NSManagedObject] = []
+    @IBOutlet weak var cotacaoAtual: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var umaSemana: UIButton!
+    @IBOutlet weak var duasSemanas: UIButton!
+    @IBOutlet weak var umMes: UIButton!
+    @IBOutlet weak var tresMeses: UIButton!
+    @IBOutlet weak var seisMeses: UIButton!
+    @IBOutlet weak var umAno: UIButton!
+    @IBOutlet weak var todos: UIButton!
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        self.umaSemana.layer.borderWidth = 0
+        self.duasSemanas.layer.borderWidth = 0
+        self.umMes.layer.borderWidth = 0
+        self.tresMeses.layer.borderWidth = 0
+        self.seisMeses.layer.borderWidth = 0
+        self.umAno.layer.borderWidth = 0
+        self.umaSemana.layer.borderWidth = 0
+        self.todos.layer.borderWidth = 0
+        
+        switch (sender){
+        case umaSemana:
+            self.umaSemana.layer.borderWidth = 1
+            self.getUrlJson(dia: "1week")
+        case duasSemanas:
+            self.duasSemanas.layer.borderWidth = 1
+            self.getUrlJson(dia: "2weeks")
+        case umMes:
+            self.umMes.layer.borderWidth = 1
+            self.getUrlJson(dia: "1months")
+        case tresMeses:
+            self.tresMeses.layer.borderWidth = 1
+            self.getUrlJson(dia: "3months")
+        case seisMeses:
+            self.seisMeses.layer.borderWidth = 1
+            self.getUrlJson(dia: "6months")
+        case umAno:
+            self.umAno.layer.borderWidth = 1
+            self.getUrlJson(dia: "1years")
+        default:
+            self.todos.layer.borderWidth = 1
+            self.getUrlJson(dia: "2years")
+        }
+        
+        
+    }
+    
+    
+    func buttonStyle(){
+        self.umaSemana.layer.cornerRadius = 15
+        self.duasSemanas.layer.cornerRadius = 15
+        self.umMes.layer.cornerRadius = 15
+        self.tresMeses.layer.cornerRadius = 15
+        self.seisMeses.layer.cornerRadius = 15
+        self.umAno.layer.cornerRadius = 15
+        self.todos.layer.cornerRadius = 15
+        
+        self.umaSemana.layer.borderColor = UIColor.white.cgColor
+        self.duasSemanas.layer.borderColor = UIColor.white.cgColor
+        self.umMes.layer.borderColor = UIColor.white.cgColor
+        self.tresMeses.layer.borderColor = UIColor.white.cgColor
+        self.seisMeses.layer.borderColor = UIColor.white.cgColor
+        self.umAno.layer.borderColor = UIColor.white.cgColor
+        self.todos.layer.borderColor = UIColor.white.cgColor
+        
+        self.umaSemana.layer.borderWidth = 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //configureGraph()
+        getUrlJson(dia: "1week")
         configureStatusBar()
-        getUrlJson()
+        buttonStyle()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadCoreDate()
+
+    }
+    func loadCoreDate(){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "CotacaoCore")
+        
+        do {
+            self.cotacoes = try managedContext.fetch(fetchRequest)
+            for aux in cotacoes{
+                print(aux.value(forKey: "valor") as! Double)
+                
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        self.customTableView.reloadData()
     }
     
     func configureStatusBar(){
@@ -28,13 +147,20 @@ class BitcoinViewController: UIViewController {
     
     func configureGraph(cotacao:Cotacao){
         var totalPontos = [Double]()
+        var totalDias = [String]()
+        var date = NSDate()
+        let formatter = DateFormatter()
         for total in cotacao.cordenadas{
-            totalPontos.append("%.02",total.y)
+            totalPontos.append(Double(round(1000*total.y)/1000))
+            date = NSDate(timeIntervalSince1970: total.x)
+            
+            totalDias.append(String(describing: date))
+            
         }
         aaChartView = AAChartView()
         let chartViewWidth = view.frame.size.width
         let chartViewHeight = view.frame.size.height - 420
-        aaChartView?.frame = CGRect(x: 0, y: 138, width: chartViewWidth, height: chartViewHeight)
+        aaChartView?.frame = CGRect(x: 0, y: 178, width: chartViewWidth, height: chartViewHeight)
         aaChartView?.contentHeight = chartViewHeight - 20
         view.addSubview(aaChartView!)
         aaChartView?.scrollEnabled = false//禁止图表内容滚动
@@ -64,8 +190,9 @@ class BitcoinViewController: UIViewController {
         aaChartView?.aa_drawChartWithChartModel(aaChartModel!)
     }
 
-    func getUrlJson(){
-        guard let gitUrl = URL(string: "https://api.blockchain.info/charts/market-price?timespan=31days") else { return }
+    func getUrlJson(dia: String){
+        guard let gitUrl = URL(string: "https://api.blockchain.info/charts/market-price?timespan=\(dia)") else { return }
+        print(gitUrl)
         URLSession.shared.dataTask(with: gitUrl) { (data, response
             , error) in
             guard let data = data else { return }
@@ -74,16 +201,33 @@ class BitcoinViewController: UIViewController {
                 let gitData = try decoder.decode(Cotacao.self, from: data)
                 DispatchQueue.main.async {
                     self.configureGraph(cotacao: gitData)
+                    self.cotacaoAtual.text = String(format: "USD %.2f", gitData.cordenadas[gitData.cordenadas.count - 1].y)
+                    //self.save(valor: gitData.cordenadas[gitData.cordenadas.count - 1].y, data: gitData.cordenadas[gitData.cordenadas.count - 1].x)
                 }
-                print(gitData.cordenadas[0].y ?? "Empty Name")
+                
                 
             } catch let err {
                 print("Err", err)
             }
             }.resume()
+    }
+    
+    func save(valor: Double, data: Double) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
         
-        
-        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "CotacaoCore", in: managedContext)!
+        let cotacao = NSManagedObject(entity: entity, insertInto: managedContext)
+        cotacao.setValue(valor, forKeyPath: "valor")
+        //cotacao.setValue(data, forKeyPath: "data")
+        do {
+            try managedContext.save()
+            cotacoes.append(cotacao)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
 }
 
